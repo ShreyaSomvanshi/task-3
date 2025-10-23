cf1919194d955e6cba07d2ed7e0fe050daa64088
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-10-23 19:22:15 +0530

    add changes

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ app.get("/users/:id",(req,res) => {
     return res.json(user);
 });
 
-app.post("/users",(req,res) => {
+app.post("/signup",(req,res) => {
     const body = req.body;
     users.push({...body,id:users.length+1});
     fs.writeFile("./mock_data.json" ,JSON.stringify(users), (err,data) =>{
         return res.json({status:"succes"});
     })
 })
+
+app.put("/users/:id",(req,res) => {
+    const id = req.params.id;
+    const updatedUser = req.body;
+    users.splice(id-1,1,{...updatedUser,id} )
+    res.json({ message: `User with ID ${id} updated`, user: updatedUser });
+})
+
+app.delete("/users/:id",(req,res) => {
+    const id = Number(req.params.id);
+    users.splice(id-1,1)
+    res.json({ message: `  your account has been deleted` });
+})
 app.listen(PORT, () => console.log("server started"));
 //const myServer = http.createServer(app);
-//myServer.listen(8000,() => console.log("server started"));
+//myServer.listen(8000,() => console.log("server started"))
